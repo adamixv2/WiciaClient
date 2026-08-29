05b7b63a99f70c5f84961cb9b989b1237bdeb60d
adamixv2 <adambudzisz9@gmail.com>
2026-08-29 21:47:26 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import os
 from datetime import datetime
 
 # ==================== KONFIGURACJA ====================
-TOKEN = "MTU0MzI1OTEyOTY2NDExMDY1Mg.GAp_vj.A1gAQdGxCkjmPaU-vq-_32Gb2HflEl02H8QtIY"
+TOKEN = TOKEN = os.getenv("DISCORD_TOKEN")
 OWNER_ID = 1042355571178868847
 KEYS_FILE = "keys.json"
 # ======================================================
